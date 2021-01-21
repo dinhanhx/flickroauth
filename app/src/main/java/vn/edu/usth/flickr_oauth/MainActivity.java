4e1ba9184dff9058ecb3c85d6fae9d7e7c3f1b6f
Dinh Anh <dinhanhx@gmail.com>
2021-01-21 14:47:53 +0700

    OAuth now works

--- app/src/main/java/vn/edu/usth/flickr_oauth/MainActivity.java
+++ app/src/main/java/vn/edu/usth/flickr_oauth/MainActivity.java
@@ -4,50 +4,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.FlickrException;
 import com.flickr4java.flickr.REST;
+import com.flickr4java.flickr.auth.Auth;
 import com.flickr4java.flickr.auth.AuthInterface;
 import com.flickr4java.flickr.auth.Permission;
+import com.github.scribejava.core.model.OAuth1AccessToken;
 import com.github.scribejava.core.model.OAuth1RequestToken;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private String apiKey = "b73450b170505cfc1020ef349a0a8cb4";
     private String sharedSecret = "679e34e410b67118";
     private Flickr flickr = new Flickr(apiKey, sharedSecret, new REST());
+    AuthInterface authInter = flickr.getAuthInterface();
+    OAuth1RequestToken requestToken;
+    Auth auth;
+
+    Button submit;
+    EditText tokenBox;
+    TextView tokenView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        submit = (Button) findViewById(R.id.token_submit);
+        tokenBox = (EditText) findViewById(R.id.token_box);
+        tokenView = (TextView) findViewById(R.id.token_view);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String token = tokenBox.getText().toString();
+                // Do your magic here
+                AsyncTask<Void, Void, Void> authTask = new AsyncTask<Void, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(Void... voids) {
+                        OAuth1AccessToken accessToken = (OAuth1AccessToken) authInter.getAccessToken(requestToken, token);
+                        try {
+                            auth = authInter.checkToken(accessToken);
+                        } catch (FlickrException e) {
+                            e.printStackTrace();
+                        }
+                        Log.i("succeed", auth.getUser().getUsername());
+                        return null;
+                    }
+                };
+                authTask.execute();
+                tokenView.setText(token);
+            }
+        });
+
         flickrTask ft = new flickrTask();
         ft.execute();
     }
 
-    public void sendMessage(View view) {
-        EditText tokenBox = (EditText) view.findViewById(R.id.token_box);
-        String token = tokenBox.getText().toString();
-
-        TextView userInfo = (TextView) view.findViewById(R.id.user_info);
-        userInfo.setText(token);
-    }
 
     private class flickrTask extends AsyncTask<Void, Void, String> {
 
         @Override
         protected String doInBackground(Void... voids) {
-            AuthInterface auth = flickr.getAuthInterface();
-            OAuth1RequestToken requestToken = auth.getRequestToken();
-            return auth.getAuthorizationUrl(requestToken, Permission.DELETE);
+            requestToken = authInter.getRequestToken();
+            return authInter.getAuthorizationUrl(requestToken, Permission.DELETE);
         }
 
         @Override
